e6f2d3e6dc085a192a56f95c4c016aafa2c9e959
Akihisa Tokuda <akihisa.tokuda@g.softbank.co.jp>
2021-03-30 20:24:10 +0900

    fix

--- gcp/shell/all_execute.sh
+++ gcp/shell/all_execute.sh
@@ -1,13 +1,14 @@
 sh XXX.sh # すいません読み取れませんでした。
-gcloud compute ssh --project controller-0 --command 'sh no8-contoroller012-first.sh'
-gcloud compute ssh --project controller-1 --command 'sh no8-contoroller012-first.sh'
-gcloud compute ssh --project controller-2 --command 'sh no8-contoroller012-first.sh'
+
+gcloud compute ssh controller-0 --command 'sh no8-contoroller012-first.sh'
+gcloud compute ssh controller-1 --command 'sh no8-contoroller012-first.sh'
+gcloud compute ssh controller-2 --command 'sh no8-contoroller012-first.sh'
 
 sh no8-contoroller012-second.sh
 
-gcloud compute ssh --project worker-0 --command 'sh no9-contoroller012.sh'
-gcloud compute ssh --project worker-1 --command 'sh no9-contoroller012.sh'
-gcloud compute ssh --project worker-2 --command 'sh no9-contoroller012.sh'
+gcloud compute ssh worker-0 --command 'sh no9-contoroller012.sh'
+gcloud compute ssh worker-1 --command 'sh no9-contoroller012.sh'
+gcloud compute ssh worker-2 --command 'sh no9-contoroller012.sh'
 
 sh no10-mac.sh
 # すいません読めませんでしたが、おそらくmac側で実行するシェルを追記ください。
